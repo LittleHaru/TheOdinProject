f4092f233403ba20a6b0995425bf803c797a6758
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-10 18:36:18 +0800

    refactor: simplified getComputerChoice() function
    removed if/else statements and implemented indexing a list to get a choice from math.random

--- JavaScript-Basics/Simple-Game/script.js
+++ JavaScript-Basics/Simple-Game/script.js
@@ -3,14 +3,9 @@ let computerScore = 0
 let tie = 0
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3)
-    if (choice == 0) {
-        return "rock"
-    } else if (choice == 1) {
-        return "paper"
-    } else if (choice == 2) {
-        return "scissor"
-    }
+    var choices = ["paper","rock","scissor"]
+    var word_index = Math.floor(Math.random()*choices.length)
+    return choices[word_index]
 }
 
 function getHumanChoice() {
