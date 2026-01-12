0317f46ff636de203fe85fc37902c28a26265499
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-12 17:51:48 +0800

    feat: added css and UI to rock-paper-scissors
    functions are still underway

--- JavaScript-Basics/Simple-Game/script.js
+++ JavaScript-Basics/Simple-Game/script.js
@@ -8,11 +8,6 @@ function getComputerChoice() {
     return choices[word_index]
 }
 
-function getHumanChoice() {
-    input = prompt("Enter your Choice").toLocaleLowerCase()
-    return input
-}
-
 function playRound(humanChoice,computerChoice) {
     if (humanChoice == computerChoice){
         tie++
@@ -33,14 +28,39 @@ function playRound(humanChoice,computerChoice) {
     }
 }
 
-function playGame() {
-    n = 0
-    while (n != 5) {
-        const humanChoice = getHumanChoice()
-        const computerChoice = getComputerChoice()
-        let play = playRound(humanChoice,computerChoice)
-        console.log(play)
-        n++
-    } return "Game Over: " + humanScore + " : " + computerScore + " Tied Score: " + tie
+// function playGame() {
+//     n = 0
+//     while (n != 5) {
+//         const humanChoice = getHumanChoice()
+//         const computerChoice = getComputerChoice()
+//         let play = playRound(humanChoice,computerChoice)
+//         console.log(play)
+//         n++
+//     } return "Game Over: " + humanScore + " : " + computerScore + " Tied Score: " + tie
+// }
+
+const buttons = document.querySelectorAll("button")
+const board = document.querySelector("#score")
+const outcome = document.createElement("div")
+const score = document.createElement("div")
+board.appendChild(score)
+board.appendChild(outcome)
+
+function alertGame() {
+    alert("Game Over")
 }
-console.log(playGame())
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let computerChoice = getComputerChoice()
+        let humanChoice = button.value
+        result = playRound(humanChoice,computerChoice)
+        outcome.textContent = result
+        score.textContent = humanScore + " VS " + computerScore + " Tie: " + tie
+    })
+})
+
+
+
+
+
