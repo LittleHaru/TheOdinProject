146d43e36fae43623ff66775d77d42b50587540a
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-12 15:59:19 +0800

    feat: simple shopping list using DOM Manipulation

---
+++ DOM/shoppingList.js
@@ -0,0 +1,25 @@
+const list = document.querySelector("ul")
+const button = document.querySelector("button")
+const input = document.querySelector("input");
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const myItem = input.value;
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.append(listText);
+    listText.textContent = myItem;
+    listItem.append(listBtn);
+    listBtn.textContent = "Delete";
+    listBtn.style.marginLeft = "10px"
+    list.append(listItem);
+
+    listBtn.addEventListener("click", () => {
+        list.removeChild(listItem)
+    });
+    input.focus()
+});
