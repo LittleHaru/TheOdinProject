fcbf41cb8f01b3a86c67613529d51534b9d6d0d5
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-13 11:11:49 +0800

    feat: implemented function to create sizexsize grid

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,28 +1,31 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".create")
 
-function createDiv() {
-    const div = document.createElement("div");
-    div.textContent = "hi"
-    div.style.border = "2px solid black"
-    div.style.maxHeight = "50px"
-    div.style.maxWidth = "50px"
-    container.append(div)
-    console.log("Div Created!")
-}
-
 button.addEventListener("click", () => {
     size = prompt("Enter The Size of Grid Wanted (Max 100)")
     if (size > 100) {
         alert("Max Size Reached")
-        size === 10
+        size === 0
     } else {
         createGrid(size)
     }
 })
 
 function createGrid(size) {
-    for (let i = 0; i < size; i++) {
-        createDiv(size)
+    for (let i = 0; i < size; i++) { // columns
+        const column = document.createElement("div");
+        column.style.maxHeight = "100px"
+        column.style.maxWidth = "100px"
+        column.classList.add("column")
+        for (let j = 0; j < size; j++) {
+            const row = document.createElement("div");
+            row.style.border = "2px solid black"
+            row.style.maxHeight = "100px"
+            row.style.maxWidth = "100px"
+            row.style.padding = "50px"
+            row.classList.add("column")
+            column.appendChild(row)
+        }
+        container.append(column)
     }
 }
