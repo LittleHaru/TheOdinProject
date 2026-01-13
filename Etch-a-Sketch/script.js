77098db5368e815285894a68ce5efc435199366e
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-13 12:52:33 +0800

    fix: refactored css to have dynamic screen resizing feat: implemented trail drawing with eventlistener (Event Bubbling)

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -5,27 +5,29 @@ button.addEventListener("click", () => {
     size = prompt("Enter The Size of Grid Wanted (Max 100)")
     if (size > 100) {
         alert("Max Size Reached")
-        size === 0
+        return
     } else {
         createGrid(size)
     }
 })
 
 function createGrid(size) {
-    for (let i = 0; i < size; i++) { // columns
+    container.innerHTML = ""
+    for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
-        column.style.maxHeight = "100px"
-        column.style.maxWidth = "100px"
         column.classList.add("column")
         for (let j = 0; j < size; j++) {
             const row = document.createElement("div");
-            row.style.border = "2px solid black"
-            row.style.maxHeight = "100px"
-            row.style.maxWidth = "100px"
-            row.style.padding = "50px"
-            row.classList.add("column")
+            row.classList.add("row")
             column.appendChild(row)
         }
         container.append(column)
     }
 }
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("row")) {
+        e.target.style.backgroundColor = "grey"
+    }
+})
+
