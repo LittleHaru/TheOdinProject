88667400b57dacc6a4c0ceb122923b2a27bfcd8f
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-01-16 09:49:13 +0800

    learn: array methods using objects (key and values)

--- JavaScript-Basics/Array-Methods/array-exercises.js
+++ JavaScript-Basics/Array-Methods/array-exercises.js
@@ -62,3 +62,66 @@ function filterUnique(arr) {
     return unique
 }
 console.log(filterUnique(WORD_ARR))
+
+//map to names
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+// let users = [ john, pete, mary ];
+// let names = users.map(item => item);
+// alert(names)
+
+//map to objects
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+// let users = [john, pete, mary]
+// let usersMapped = users.map(user => ({
+//     fullName: `${user.name} ${user.surname}`,
+//     id: user.id
+// }))
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+
+// sort by names
+// function sortByAge(users) {
+//     arr.sort((a,b) => a.age - b.age)
+// }
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+// let arr = [ pete, john, mary ];
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+//get average of ages
+// function getAverageAge(users) {
+//     return users.reduce((prev, user) => prev + user.age, 0) / users.length
+// }
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+
+//create keyed object from array
+function groupById(users) {
+    return users.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    }, {})
+}
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
